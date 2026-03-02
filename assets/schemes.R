64599587a60a3ffb3a7d51d2867efffc27ec01be
infinity-a11y <marian-freisleben@outlook.com>
2026-03-02 13:39:40 +0100

    Adding support for new schemes

--- assets/schemes.R
+++ assets/schemes.R
@@ -1,5 +1,10 @@
-pubmlst_schemes <- read.csv("./assets/pubmlst_schemes.csv")[, -1]
+pubmlst_schemes <- read.csv("./assets/pubmlst_schemes.csv")[, -1] |>
+  dplyr::mutate(species = paste0(species, "_PM"), database = "PubMLST") |>
+  dplyr::arrange(species)
 
-cgmlstorg_schemes <- read.csv("./assets/cgmlst_schemes.csv")[, -1]
+cgmlstorg_schemes <- read.csv("./assets/cgmlst_schemes.csv")[, -1] |>
+  dplyr::mutate(species = paste0(species, "_CM"), database = "cgMLST.org") |>
+  dplyr::arrange(species)
+
 
 amrfinder_species <- read.csv("./assets/amrfinder_species.csv")[, -1]
